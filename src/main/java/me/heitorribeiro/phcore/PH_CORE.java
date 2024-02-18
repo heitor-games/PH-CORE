ea22a40029a356aa983e43ac36434d475669cdaf
heitor-games1237 <heitor.manhaes@hotmail.com>
2024-02-18 13:35:39 -0300

    Initial Commit

---
+++ src/main/java/me/heitorribeiro/phcore/PH_CORE.java
@@ -0,0 +1,17 @@
+package me.heitorribeiro.phcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PH_CORE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
